32c98a11662aa249f8d134937442d0cc245d4f5c
Erlita Amelia <149795568+erlitaal@users.noreply.github.com>
2026-03-02 12:33:13 +0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -11,8 +11,6 @@ import { participants } from "./data/participants";
 import { meetweek1 } from "./data/meet-week1";
 import { scoreEngine } from "./utils/scoreEngine";
 
-import { meetweek1 } from "./data/meet-week1";
-
 export default function App() {
   const [season, setSeason] = useState(1);
 
