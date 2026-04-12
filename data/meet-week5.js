1c38fd63b861e93b2115470102348b8601a024e2
Erlita Amelia <149795568+erlitaal@users.noreply.github.com>
2026-04-12 18:48:30 +0700

    Update meet-week5.js

--- data/meet-week5.js
+++ data/meet-week5.js
@@ -1,1 +1,15 @@
-
+export const meetweek5 = [
+    { id: "DW-151", quizCorrect: 11, feedback: true, activityCount: 0 }, //Salwa
+    { id: "DW-364", quizCorrect: 15, feedback: true, activityCount: 0 }, //Saeful
+    { id: "DW-852", quizCorrect: -1, feedback: false, activityCount: 0 }, //Yelsa
+    { id: "DW-357", quizCorrect: -1, feedback: false, activityCount: 0 }, //Naufal Faishal
+    { id: "DW-090", quizCorrect: 13, feedback: true, activityCount: 0 }, //Ahsan
+    { id: "DW-136", quizCorrect: -1, feedback: false, activityCount: 0 }, //Asyraf
+    { id: "DW-210", quizCorrect: -1, feedback: false, activityCount: 0 }, //Andini
+    { id: "DW-097", quizCorrect: 14, feedback: true, activityCount: 1 }, //Tubagus
+    { id: "DW-999", quizCorrect: -1, feedback: false, activityCount: 0 }, //Irsyad
+    { id: "DW-027", quizCorrect: -1, feedback: false, activityCount: 0 }, //Taswa
+    { id: "DW-999", quizCorrect: -1, feedback: false, activityCount: 0 }, //Satrio
+    { id: "DW-666", quizCorrect: -1, feedback: false, activityCount: 0 }, //Keanu
+    { id: "DW-016", quizCorrect: -1, feedback: false, activityCount: 0 }, //Luthfi
+];
