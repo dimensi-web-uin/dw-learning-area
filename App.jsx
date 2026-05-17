bf04456f60a1a7df6ac4570efd0e80b4c8ca1c13
Erlita Amelia <149795568+erlitaal@users.noreply.github.com>
2026-05-17 21:24:59 +0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -16,6 +16,7 @@ import { meetweek5 } from "./data/meet-week5";
 import { meetweek6 } from "./data/meet-week6";
 import { meetweek7 } from "./data/meet-week7";
 import { meetweek8 } from "./data/meet-week8";
+import { meetweek9 } from "./data/meet-week9";
 import { scoreEngine } from "./utils/scoreEngine";
 
 export default function App() {
@@ -25,7 +26,7 @@ export default function App() {
   const activeId = window.location.hash.replace("#", "");
 
   const user = scoreEngine(activeId, participants, [
-    meetweek1, meetweek2, meetweek3, meetweek4, meetweek5, meetweek6, meetweek7, meetweek8,  
+    meetweek1, meetweek2, meetweek3, meetweek4, meetweek5, meetweek6, meetweek7, meetweek8, meetweek9, 
   ]);
 
   if (!user) return <h2>User not found</h2>;
@@ -62,7 +63,7 @@ export default function App() {
             dailyContribution={user.dailyContribution}
             dwPoint={user.totalDwPoint}
           />
-          <Leaderboard participants={participants} meetings={[meetweek1, meetweek2, meetweek3, meetweek4, meetweek5, meetweek6, meetweek7, meetweek8]} />
+          <Leaderboard participants={participants} meetings={[meetweek1, meetweek2, meetweek3, meetweek4, meetweek5, meetweek6, meetweek7, meetweek8, meetweek9]} />
 
           <FooterDW />
         </div>
